fbec1194c8d2ff502abb5f2763447877306c57e4
amaurisdominguez <arq.a.dgz@gmail.com>
2024-05-31 01:36:10 -0400

    Enhancement add react photo sphere viewer (#11)
    * update: packages include react-photo-sphere-viewer
    
    * Create SALA.jpg
    
    * Update Home.tsx
    
    * Create ThreeSixtyView.tsx
    
    * Update Routes.tsx
    
    * update: link test
    
    ---------
    
    Co-authored-by: Ruben Dominguez <argenis.dominguez@hotmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-    return (
-        <h1 className="text-xl">Home</h1>
-    )
+	return (
+		<div className="container mx-auto">
+			<h1 className="text-xl">Home</h1>
+			<div className="p-6">
+				<p className="font-bold">360 Viewer Test</p>
+				<ul className="text-blue-500">
+					<li>
+						<Link to="/v360/view">Sala</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,12 +3,14 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
+import ThreeSixtyView from "../pages/ThreeSixtyView";
 
 export default function Routes() {
     const elements = useRoutes([
         {path: '/v360/', element: <Home />},
         {path: '/v360/about', element: <About />},
         {path: '/v360/Contact', element: <Contact />},
+        {path: '/v360/view', element: <ThreeSixtyView />},
         {path: '*', element: <NotFound />},
     ])
 
