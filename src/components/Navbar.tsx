7ecb416570dc105a9d25f6c0d161572c8abc6f94
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-05-30 23:07:51 -0400

    update: navbar implement icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
+import { faFacebookF, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-    return (
+	return (
 		<nav className="flex flex-row px-4 py-6 bg-gradient-to-r from-cyan-500 to-blue-500 text-white">
 			<div>
 				<Link to="/v360/">
@@ -31,18 +33,24 @@ export default function Navbar() {
 			<div>
 				<ul className="flex flex-row space-x-4 ml-10">
 					<li>
-						<a href="/" target="_blank">
-							linkedin
+						<a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+							<div className="rounded-full text-center" style={{ border: '2px solid white', height: '32px', width: '32px', paddingTop: '2px' }}>
+								<FontAwesomeIcon icon={faLinkedinIn} />
+							</div>
 						</a>
 					</li>
 					<li>
-						<a href="/" target="_blank">
-							facebook
+						<a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+							<div className="rounded-full text-center" style={{ border: '2px solid white', height: '32px', width: '32px', paddingTop: '2px' }}>
+								<FontAwesomeIcon icon={faFacebookF} />
+							</div>
 						</a>
 					</li>
 					<li>
-						<a href="/" target="_blank">
-							instagram
+						<a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+							<div className="rounded-full text-center" style={{ border: '2px solid white', height: '32px', width: '32px', paddingTop: '2px' }}>
+								<FontAwesomeIcon icon={faInstagram} />
+							</div>
 						</a>
 					</li>
 				</ul>
