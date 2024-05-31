26a2cdd20c0e11b53bdcc3b27666ea635ef3d354
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-05-30 22:31:17 -0400

    create: not found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <>
+            <h1 className="text-xl">404</h1>
+            <p>The page you were looking for was not found</p>
+        </>
+    )
+}
