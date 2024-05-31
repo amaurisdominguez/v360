a98906aa9db6f84a54af57683869fff8ec0b94dd
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-05-30 22:30:31 -0400

    update: app to add router navbar and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Routes from './routes/Routes';
+
 function App() {
 	return (
-    <header>
-      <div className="flex flex-row">
-        <h1 className="text-3xl font-bold">v360</h1>
-        <div className="grow"></div>
-        <div>
-          <ul className="flex flex-row space-x-4">
-            <li>linkedin</li>
-            <li>facebook</li>
-            <li>instagram</li>
-          </ul>
-        </div>
-      </div>
-    </header>
-  )
+		<Router>
+			<Navbar />
+			<Routes />
+		</Router>
+	);
 }
 
 export default App;
